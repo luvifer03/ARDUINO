13392706a2e1492b29de51cfb8d5f57f07862cae
Aashish Solanky <aashishsolanki1@gmail.com>
2019-09-15 23:05:18 +0530

    Add files via upload

---
+++ Assets/updown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class updown : MonoBehaviour {
+	SerialPort sp = new SerialPort ("\\\\.\\COM11", 9600);
+	// Use this for initialization
+	void Start () {
+		sp.Open();
+		sp.ReadTimeout = 45;
+	}
+
+	void Update () {
+		if (sp.IsOpen) {
+			try {
+				if (sp.ReadByte() == 1) {
+					transform.Translate(Vector3.down * Time.deltaTime * 5);
+				}
+				if (sp.ReadByte() == 2) {
+					transform.Translate(Vector3.up * Time.deltaTime * 5);
+				}
+			} catch (System.Exception) {
+			}
+		}
+	}
+}
